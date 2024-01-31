b933aae79f7ddb64d184b3fd2fb965f65b18eafd
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-01-31 14:25:37 -0800

    feat: add markdown plugins

--- config/plugins/markdown.js
+++ config/plugins/markdown.js
@@ -1,8 +1,20 @@
 const markdownIt = require("markdown-it");
+const markdownItFootnote = require("markdown-it-footnote");
+const markdownItPrism = require("markdown-it-prism");
+const markdownItAbbr = require("markdown-it-abbr");
+const markdownItAnchor = require("markdown-it-anchor");
 
-module.exports = markdownIt({
+const markdown = markdownIt({
   html: true,
   breaks: true,
   linkify: true,
   typographer: true,
-});
+})
+  .use(markdownItFootnote)
+  .use(markdownItAbbr)
+  .use(markdownItAnchor)
+  .use(markdownItPrism, {
+    defaultLanguage: "plaintext",
+  });
+
+module.exports = markdown;

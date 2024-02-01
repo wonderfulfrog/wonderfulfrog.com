23c9baad107d5a7a7ff72b8c05f7457991d38a5d
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-02-01 15:50:39 -0800

    feat: move color variables to global data

--- config/filters/index.js
+++ config/filters/index.js
@@ -5,7 +5,7 @@ const values = Object.values;
 const entries = Object.entries;
 
 const minifyCss = async (css) => {
-  const output = await postcss([cssnano]).process(css);
+  const output = await postcss([cssnano]).process(css, undefined);
 
   return output.css;
 };
---
+++ src/data/colors.js
@@ -0,0 +1,28 @@
+/**
+ * Color namer https://colornamer.robertcooper.me/
+ */
+
+const colors = {
+  aqua: {
+    50: "#aae9d2",
+    100: "#99e5ce",
+    200: "#79ddcc",
+    300: "#58d4d0",
+    400: "#38bdcc",
+    500: "#2c8daa",
+    600: "#226186",
+    700: "#193d61",
+    800: "#10203d",
+    900: "#060a18",
+    950: "#020206",
+  },
+  carotte: {
+    400: "#ed5215",
+  },
+  neutrals: {
+    white: "#ffffff",
+    black: "#000000",
+  },
+};
+
+module.exports = colors;

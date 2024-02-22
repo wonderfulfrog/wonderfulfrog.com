47d015eedbc055d2692af63fa7cb6d25f2364db7
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-02-22 15:15:00 -0800

    fix: book not showing author

--- src/content/catalogue/books/books.11tydata.js
+++ src/content/catalogue/books/books.11tydata.js
@@ -3,4 +3,8 @@ module.exports = {
   tags: "book",
   permalink: "catalogue/books/{{ page.fileSlug }}/index.html",
   linkTitle: "View book details",
+  eleventyComputed: {
+    tertiary: (data) =>
+      `<p style="--flow-space: 0.5em"><span class="[ text-fadeText ]">by</span> ${data.author}</p>`,
+  },
 };

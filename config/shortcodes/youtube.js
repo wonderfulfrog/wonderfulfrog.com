91bf9a1d7bcd6bc119b22166f9740506ef79fe1f
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-02-28 22:36:37 -0800

    feat: small fixes

--- config/shortcodes/youtube.js
+++ config/shortcodes/youtube.js
@@ -2,13 +2,13 @@
  * https://github.com/madrilene/eleventy-excellent/blob/main/config/shortcodes/youtube-lite/index.js
  */
 const liteYoutube = (id, label) => {
-  return `
-  <div class="youtube-embed"> <lite-youtube videoid="${id}" style="background-image: url('https://i.ytimg.com/vi/${id}/hqdefault.jpg');">
+  return `<div class="youtube-embed">
+<lite-youtube videoid="${id}" style="background-image: url('https://i.ytimg.com/vi/${id}/hqdefault.jpg');">
   <button type="button" class="lty-playbtn">
     <span class="lyt-visually-hidden">${label}</span>
   </button>
-</lite-youtube></div>
-    `;
+</lite-youtube>
+</div>`;
 };
 
 module.exports = liteYoutube;
